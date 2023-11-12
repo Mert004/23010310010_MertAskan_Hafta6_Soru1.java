3c8894662c3f24e912d938866b5797cea6542aad
merta <merta@10.57.51.165>
2023-11-12 12:42:36 +0300

    23010310010_MertAşkan_Hafta6_Soru1.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+        while(i < 10) {
+            System.out.println("Merhaba");
+            i++;
+        }
+    }
+}
